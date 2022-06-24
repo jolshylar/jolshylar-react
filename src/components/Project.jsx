db62ec00c68580bd100e2d1d9d4f6d91df0c0b9a
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-06-24 15:50:31 +0600

    🚀 Data fetching improvements

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,23 +1,30 @@
 import "../styles/Project.css";
 import { BiLinkExternal } from "react-icons/bi";
+import { AiOutlineStar } from "react-icons/ai";
 
 function Project({ project }) {
   const getPreview = (name) => {
     try {
-      return require(`../images/previews/${name}.png`)
+      return require(`../images/previews/${name}.png`);
     } catch (err) {
-      return require('../images/previews/unavailable.png')
+      return require("../images/previews/unavailable.png");
     }
-  }
+  };
 
   return (
     <div className="project-card">
-      <img className="preview" alt="project preview" src={getPreview(project.name)} />
+      <img
+        className="preview"
+        alt="project preview"
+        src={getPreview(project.name)}
+      />
       <div className="content">
         <h2>{project.name}</h2>
         <div className="content--info">
           <ul>
-            <li>Stars: {project.stargazers_count} ⭐️</li>
+            <li>
+              Stars: {project.stargazers_count} <AiOutlineStar />
+            </li>
             <li>Language: {project.language}</li>
           </ul>
         </div>
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,32 +1,16 @@
 import "../styles/ProjectList.css";
 import Project from "./Project";
-import { useState, useEffect } from "react";
+import data from "../data/project";
 
 function ProjectList() {
-  const [repos, setRepos] = useState();
-  const ignoredRepoNames = [".github", "jolshylar"];
-
-  const getGithubRepos = async () => {
-    const response = await fetch(
-      "https://api.github.com/users/jolshylar/repos"
-    ).then((response) => response.json());
-
-    const repositories = response.filter(
-      (repo) => !ignoredRepoNames.includes(repo.name)
-    );
-    setRepos(repositories);
-  };
-
-  useEffect(() => {
-    getGithubRepos();
-  });
-
   return (
     <div className="projects">
       <h1 className="members--title">Our Projects</h1>
       <div className="project-container">
         <div className="project-list">
-          {repos && repos.map((repo) => <Project project={repo} />)}
+          {data.map((repo) => (
+            <Project key={repo.name} project={repo} />
+          ))}
         </div>
       </div>
     </div>
